aed913f6e45cc443f0edb5edc0c0d52584e2c862
Your Name <you@example.com>
2021-05-29 23:21:28 +0100

    made change

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("June 1, 2021 00:00:00").getTime();
+    const countDate = new Date("June 20, 2021 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
